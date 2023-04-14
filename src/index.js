72b5f7aa17a14fa0b5e568f711c5fc885f3fafd5
Chimankpa Osuji <chimankpaosuji@gmail.com>
2023-04-14 09:03:00 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -9,30 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-{/* <Form>
-                    <label>Faculty</label>
-                    <select value={myCar} onChange={handleChange}>
-                      <option value="ford">Faculty of Science</option>
-                      <option value="volvo">Faculty of Technology</option>
-                    </select>
-                  </Form>
-
-                  <Dropdown>
-                    <Dropdown.Toggle
-                      as={CustomToggle}
-                      id="dropdown-basic"
-                      label="Custom Components"
-                      title="Faculties"
-                      // variant="success"
-                      onChange={handleChanges}
-                    >
-                      Faculties
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu as={CustomMenu}>
-                      {faculties.myarray.map((data) => (
-                        <Dropdown.Item key={data.id}>{data}</Dropdown.Item>
-                      ))}
-                    </Dropdown.Menu>
-                  </Dropdown> */}
